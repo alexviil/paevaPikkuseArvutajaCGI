6066fca87649d90ee9ddf2dfb4b3811eb3bd8233
Alex <alexviil145@live.com>
2020-04-20 14:15:30 +0300

    Teine etapp

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Päeva Pikkuse Arvutaja (CGI ülesanne)
-## Autor: Alex Viil
+# Päeva Pikkuse Arvutaja (CGI ülesanne 2020)
+#### Autor: Alex Viil
+
+## Rakendusest
+Kirjeldav tekst
+
+## Käivitus
+Kuna tegu on eraldi Javascripti käituskeskonnata veebirakendusega, siis käivitamiseks 
+piisab ***index.html*** faili avamisest brauseris. Ei pruugi vanemates brauserites 
+(Internet Explorer 8 ja vanem, jms) täielikult töötada.  
+Võimalik on ka interneti kaudu otse proovida, ilma et peaks projekti kaustas sorama.  
+[Proovige rakendust otse veebis, vajutades siia!](http://kodu.ut.ee/~alexviil/)
+
+
+## Tööks kulunud aeg, raskused
+
+### Etapp 1
+***Ajakulu: 3 tundi***  
+  
+Väga lihtne oli nullist luua toores ja lihtne HTML+CSS+Javascript rakendus, mis
+võtaks kasutajalt sisendi ja sellega midagi teeks (Kahjuks pole nii palju aega 
+vaheeksamite ja kodutööde kõrvalt, et node.js meelde tuletada, õnneks väike rakendus
+ka).  
+
+Keeruliseks osutus aga päikesetõusu ja -loojangu arvutamine koordinaatide alusel. 
+Selleks leidus valemeid mitmest allikast, kuid need olid pikad ja veatuses oli raske
+veenduda (ja lisaks ka suur ajaline kulu, et need suured valemid kirja panna). Oli ka
+lihtsamaid valemeid, aga need töötasid oletusel, et maa on perfektne kera (mõistagi,
+tegelikult ei ole perfektne, kuid on keralaadne küll), ning nendega oli oht kindlates 
+oludes, et annab väga vale vastuse.  
+Internetis oli aga mitu vägagi vabade litsentsidega valmis tehtud Javascripti lahendust 
+olemas. Valisin kõige kergema neist ning veendusin, et annab väga lähedasi tulemusi 
+võrreldes täpsete allikatega.  
+
+Kellaaegadega toimetamine (UTC versus kohalik) ei olnud liialt keeruline aga samas ka
+mitte lihtne. Leidsin ka selleks kergekaalulise Javascripti lahenduse vaba litsentsiga, 
+mis koordinaatide alusel annab ajatsooni kätte kujul, mis sisseehitatud Date'ile korda 
+läheb.
+
+### Etapp 2
+***Ajakulu: 2 tundi***
+
+Teine etapp tundus esialgu raske, kuid osutus üllatusväärselt kergeks. Algselt pidin 
+otsustama, millist teeki kasutada. Lisalugemise alt paistis OpenLayers kohe silma, kuna
+olin sellest varem kuulnud, kuid igaks juhuks vaatasin ka Leafletjs-i üle. Leafletjs 
+tundus palju lihtsama ja kergekaalulisema ülesehitusega olevat, seega otsustasin tema
+kasuks. 
+
+Leafletjs-i teegil oli ka väga põhjalik kuid samas lühike dokumentatsioon, mis 
+osutus väga suureks abiks ning aitas kiiresti etapi läbida.
--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@
 Help used:
 https://en.wikipedia.org/wiki/Sunrise_equation
 https://stackoverflow.com/questions/11759992/calculating-jdayjulian-day-in-javascript
+But I decided against the time expensive method of writing a huge equation in a domain I'm not familiar with
 
 SunCalc's license allows for commercial and private use. It seems to be quite accurate and trustworthy.
 https://github.com/mourner/suncalc
@@ -28,18 +29,28 @@ function calculate() {
     let SCsunriseLCL = new Date(SCsunriseUTC.toLocaleString("en-US", {timeZone: tzlookup(latitude, longitude)}));
     let SCsunsetLCL = new Date(SCsunsetUTC.toLocaleString("en-US", {timeZone: tzlookup(latitude, longitude)}));
 
-
+    // UTC and Local sunrise, sunset
     let srUTC = getTime(SCsunriseUTC, true);
     let srLCL = getTime(SCsunriseLCL, false);
 
     let ssUTC = getTime(SCsunsetUTC, true);
     let ssLCL = getTime(SCsunsetLCL, false);
 
-    let difference = Math.round((SC.sunset - SC.sunrise) / 36000) / 100;
+    let dayLength = Math.round((SC.sunset - SC.sunrise) / 36000) / 100;
+    if (isNaN(dayLength)) {
+        // North-south, Winter-summer (from Northern hemisphere's view)
+        let NS = latitude > 0;
+        let WS = date.getMonth() <= 2 || date.getMonth() > 8;
+        dayLength = " päike ei " + (NS === WS ? "tõuse" : "looju");
+    } else if (dayLength === 1.0) {
+        dayLength = dayLength + " tund"
+    } else {
+        dayLength = dayLength + " tundi"
+    }
 
     $("#result").html("Päikesetõus (UTC): " + srUTC + ", Päikeseloojang (UTC): " + ssUTC +
         " Päikesetõus (kohalik): " + srLCL + ", Päikeseloojang (kohalik): " + ssLCL +
-        ", Päeva pikkus: " + difference + " tundi");
+        ", Päeva pikkus: " + dayLength);
 
 }
 
@@ -55,6 +66,10 @@ function getTime(date, UTC) {
         hours = date.getHours();
     }
 
+    if (isNaN(minutes) || isNaN(hours)) {
+        return "XX:XX";
+    }
+
     return (hours < 10 ? "0" : "") + hours.toString() + ":" + (minutes < 10 ? "0" : "") + minutes.toString();
 }
 
@@ -64,6 +79,52 @@ $(function() {
     let day = d.getDate();
     let month = d.getMonth() + 1;
     let year = d.getFullYear();
-
     $('#date').val(year + "-" + (month < 10 ? "0" : "") + month + "-" + day);
+
+    // https://leafletjs.com/examples/quick-start/
+
+    // Initialization (includes access token I got from Mapbox)
+    var mymap = L.map('mapid').setView([58.38, 26.72], 6);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWxleHZpaWwiLCJhIjoiY2s5NXg2anpmMGFmMDNrbDRpemlmbnpyNCJ9.mlus4jEkWhmwMLV0zDVMxw', {
+        maxZoom: 14,
+        minZoom: 2,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+
+    mymap.setMaxBounds([[-90, -180], [90, 180]]);
+
+    let latitudeField = $("#latitude");
+    let longitudeField = $("#longitude");
+
+    // Click function
+    let marker = L.marker([latitudeField.val(), longitudeField.val()]);
+    marker.addTo(mymap);
+
+    mymap.on('click', function(e) {
+        marker.setLatLng(e.latlng);
+        let latitude = Math.round(e.latlng.lat * 1000.0) / 1000.0;
+        let longitude = Math.round(e.latlng.lng * 1000.0) / 1000.0;
+
+        latitudeField.val(latitude);
+        longitudeField.val(longitude);
+    });
+
+    // Listeners for input fields
+    const updateMap = (function () {
+        let latitude = latitudeField.val();
+        let longitude = longitudeField.val();
+
+        if (latitude < 90 && latitude > -90 && longitude < 180 && longitude > -180) {
+            marker.setLatLng([latitude, longitude]);
+            mymap.panTo([latitude, longitude]);
+        }
+    });
+
+    latitudeField.on("change", () => updateMap());
+    longitudeField.on("change", () => updateMap());
 });
